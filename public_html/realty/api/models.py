6fed4f30a5baefe939be09a5fc2fd6b4ed364c90
Sergey <45151835+thebbstudio@users.noreply.github.com>
2022-05-25 23:10:23 +0500

    Расписал модели, добавил маршрутизацию

--- public_html/realty/api/models.py
+++ public_html/realty/api/models.py
@@ -1,25 +1,63 @@
-from turtle import title
+import email
 from django.db import models
+from datetime import datetime
 
 
 class Role(models.Model):
-    title = models.CharField(max_length=25)
+    name = models.CharField(max_length=25)
+
+
+class Token(models.Model):
+    token = models.CharField(max_length=50)
+    isActive = models.BooleanField(default=True)
+    sellByUTC = models.DateTimeField(defalt=datetime.utcnow())
+
 
 class User(models.Model):
     login = models.CharField(max_length=50)
-    
+    password = models.CharField(max_length=20)
+    isActive = models.BooleanField(default=True)
+    role = models.ForeignKey(Role, on_delete=models.CASCADE)
+    token = models.ForeignKey(Token, on_delete=models.CASCADE)
+
+
+# Каталоги с данными
+class CatalogSource(models.Model):
+    name = models.CharField(max_length=50)
+
+
+class CatalogTypeField(models.Model):
+    name = models.CharField(max_length=50)
+
+
+class CatalogFormField(models.Model):
+    typeField = models.ForeignKey(CatalogTypeField, on_delete=models.CASCADE)
+    name = models.CharField(max_length=20)
+    label = models.CharField(max_length=20, blank=True)
+    required = models.BooleanField(default=True)
+    placehplder = models.CharField(max_length=20, blank=True)
+
+
+class CatalogOptions(models.Model):
+    name = models.CharField(max_length=50)
+    form = models.ForeignKey(CatalogFormField, on_delete=models.CASCADE)
+
 
 class Owner(models.Model):
-    pass
+    fullName = models.CharField(max_length=255)
+    phone = models.CharField(max_length=12)
+    email = models.CharField(max_length=50)
+    source = models.ForeignKey(CatalogSource, on_delete=models.CASCADE)
+
 
 class ObjectData(models.Model):
-    pass
+    owner = models.ForeignKey(Owner, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    key = models.CharField(max_length=25)
+    value = models.CharField(max_length=200)
+    isActive = models.BooleanField(default=True)
+
 
 class ImageObject(models.Model):
-    pass
-
-class FormField(models.Model):
-    pass
-
-class DropDownValue(models.Model):
-    pass
+    objectData = models.ForeignKey(ObjectData, on_delete=models.CASCADE)
+    path = models.ImageField(upload_to = 'objects/% Y/% m/% d/', default=None, blank=True)
--- public_html/realty/api/views.py
+++ public_html/realty/api/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.core.exceptions import ObjectDoesNotExist
 
-# Create your views here.
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .models import *
+
+
+def ValidateParams(paramsList, dataList):
+    for param in paramsList:
+        if param not in dataList:
+            return Response(status=400, data={'msg' : 'Missing parameter'})
+
+class AuthView(APIView):
+    def post(self, request):
+        ValidateParams(('login', 'password'), request.data)
+        try:
+            user = User.objects.get()
+        except ObjectDoesNotExist:
+            print('Error auth')
+            user = None
+
