52031b926f33c96c286bedd025493aa62ad60f27
Sergey <45151835+thebbstudio@users.noreply.github.com>
2022-05-25 20:48:15 +0500

    Добавление в докумендацию данных

--- public_html/realty/api/models.py
+++ public_html/realty/api/models.py
@@ -1,3 +1,25 @@
+from turtle import title
 from django.db import models
 
-# Create your models here.
+
+class Role(models.Model):
+    title = models.CharField(max_length=25)
+
+class User(models.Model):
+    login = models.CharField(max_length=50)
+    
+
+class Owner(models.Model):
+    pass
+
+class ObjectData(models.Model):
+    pass
+
+class ImageObject(models.Model):
+    pass
+
+class FormField(models.Model):
+    pass
+
+class DropDownValue(models.Model):
+    pass
