a2562b95fe033f63178bb07e2956eacce3633c11
Sergey <45151835+thebbstudio@users.noreply.github.com>
2022-05-26 21:48:25 +0500

    Создание документации API

--- public_html/realty/api/models.py
+++ public_html/realty/api/models.py
@@ -6,7 +6,6 @@ from datetime import datetime
 class Role(models.Model):
     name = models.CharField(max_length=25)
 
-
 class Token(models.Model):
     token = models.CharField(max_length=50)
     isActive = models.BooleanField(default=True)
@@ -14,50 +13,33 @@ class Token(models.Model):
 
 
 class User(models.Model):
+    fullName = models.CharField(max_length=255)
+
     login = models.CharField(max_length=50)
     password = models.CharField(max_length=20)
+    
     isActive = models.BooleanField(default=True)
     role = models.ForeignKey(Role, on_delete=models.CASCADE)
     token = models.ForeignKey(Token, on_delete=models.CASCADE)
-
-
-# Каталоги с данными
-class CatalogSource(models.Model):
-    name = models.CharField(max_length=50)
-
-
-class CatalogTypeField(models.Model):
-    name = models.CharField(max_length=50)
-
-
-class CatalogFormField(models.Model):
-    typeField = models.ForeignKey(CatalogTypeField, on_delete=models.CASCADE)
-    name = models.CharField(max_length=20)
-    label = models.CharField(max_length=20, blank=True)
-    required = models.BooleanField(default=True)
-    placehplder = models.CharField(max_length=20, blank=True)
-
-
-class CatalogOptions(models.Model):
-    name = models.CharField(max_length=50)
-    form = models.ForeignKey(CatalogFormField, on_delete=models.CASCADE)
 
 
 class Owner(models.Model):
     fullName = models.CharField(max_length=255)
     phone = models.CharField(max_length=12)
     email = models.CharField(max_length=50)
-    source = models.ForeignKey(CatalogSource, on_delete=models.CASCADE)
+    
 
-
-class ObjectData(models.Model):
+class Realty(models.Model):
     owner = models.ForeignKey(Owner, on_delete=models.CASCADE)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+    
+
+class RealtyData(models.Model):
+    obj = models.ForeignKey(Realty, on_delete=models.CASCADE)
     key = models.CharField(max_length=25)
     value = models.CharField(max_length=200)
-    isActive = models.BooleanField(default=True)
 
 
 class ImageObject(models.Model):
-    objectData = models.ForeignKey(ObjectData, on_delete=models.CASCADE)
+    realty = models.ForeignKey(Realty, on_delete=models.CASCADE)
     path = models.ImageField(upload_to = 'objects/% Y/% m/% d/', default=None, blank=True)
--- public_html/realty/api/views.py
+++ public_html/realty/api/views.py
@@ -4,18 +4,125 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from .models import *
 
+from datetime import datetime
+
 
 def ValidateParams(paramsList, dataList):
     for param in paramsList:
         if param not in dataList:
-            return Response(status=400, data={'msg' : 'Missing parameter'})
+            return False
+    return True
+
 
 class AuthView(APIView):
     def post(self, request):
-        ValidateParams(('login', 'password'), request.data)
+        if !(ValidateParams(('login', 'password'), request.data)):
+            return Response(status=401, data={'msg' : 'Missing parameter'})
+
         try:
             user = User.objects.get()
         except ObjectDoesNotExist:
             print('Error auth')
             user = None
+
+
+def GetDataObject(id):
+    return RealtyData.objects.filter(obj = id).values()
+    
+
+def GetUser(id, pravoHave):
+    if !(pravoHave):
+        return None
+    return User.objects.get(id=id).fullName
+
+
+class GetDataRealty(APIView):
+    def get(self,request):
+        if !(ValidateParams(('token', 'userId'), request.data)):
+            return Response(status=401, data={'msg' : 'Missing parameter'})
+        
+        # Проверка есть ли вообще такой пользователь
+        try:
+            user = User.objects.get(token=request.data['token'] and id = request.data['userId'])
+        except ObjectDoesNotExist:
+            print('Error: token or userId not found')
+            return Response(status=403, data={'msg': 'Data is not validate'})
+
+        # Проверка есть ли вообще такой токин
+        try:
+            token = Token.objects.get(id = user.token)
+        except ObjectDoesNotExist:
+            print('Error: token not found')
+            return Response(status=403, data={'msg': 'Data is not validate'})
+        
+        # Проверка живости токена
+        if token.sellByUTC < datetime.utcnow():
+            return Response(status=403, data={'msg':'Token time is up'})
+
+        if Role.objects.get(id = user.role).name in ('admin','super admin', 'manager'):
+            realties = Realty.objects.all().values()
+            pravoHave = True 
+        else:
+            realty = Realty.objects.filter(user_id = user.id).values()
+            pravoHave = False
+
+        resp = []
+        for realty in realties:
+            resp.append({'id' : realty['id'], 
+                        'userFullName': GetUser(realty['id'], pravoHave), 
+                        'data' : GetObjData(realty['id'])})
+
+        return Response(status=200, data=resp)
+
+
+def GetNewRealtyId(user)
+
+
+class CreateObj(APIView):
+    def post(self, request):
+        if !(ValidateParams(('token', 'userId','data'),request.data))
+            return Response(status=401, data={'msg' : 'Missing parameter'})
+        
+        # Проверка есть ли вообще такой пользователь
+        try:
+            user = User.objects.get(token=request.data['token'] and id = request.data['userId'])
+        except ObjectDoesNotExist:
+            print('Error: token or userId not found')
+            return Response(status=403, data={'msg': 'Data is not validate'})
+
+        # Проверка есть ли вообще такой токин
+        try:
+            token = Token.objects.get(id = user.token)
+        except ObjectDoesNotExist:
+            print('Error: token not found')
+            return Response(status=403, data={'msg': 'Data is not validate'})
+        
+        # Проверка живости токена
+        if token.sellByUTC < datetime.utcnow():
+            return Response(status=403, data={'msg':'Token time is up'})
+        
+        params = request.data['data']
+        
+        try:
+            ownerObj = param.pop('owner')
+        except Exception:
+            print('Error: object `owner` dont found')
+            return Response(status=401, data={'msg' : 'Error: object `owner` dont found'})
+
+        owner = Owner(fullName=ownerObj['fullName'], phone=ownerObj['phone'], email=ownerObj['email'])
+        owner.save()
+
+        
+        for param in params:
+            
+
+
+
+        
+        
+
+
+        
+
+        
 
