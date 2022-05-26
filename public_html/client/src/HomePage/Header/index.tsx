e7bfd877c3b0a1a741ee13dab83743e890330258
Aleksandr <mamamacriminal@yandex.ru>
2022-05-26 14:26:51 +0500

    change model inputs
    add field 'value'

---
+++ public_html/client/src/HomePage/Header/index.tsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import { Button } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+
+// TODO:
+//  - add pseudoattr for links
+
+const Header = () => (
+  <header
+    className="
+        d-flex
+        flex-wrap
+        align-items-center
+        justify-content-center
+        justify-content-md-between
+        p-3
+        mb-4
+        border-bottom
+        shadow-sm
+      "
+  >
+    <Link
+      to="/"
+      className="
+            d-flex
+            align-items-center
+            col-md-3
+            mb-2
+            mb-md-0
+            text-dark
+            text-decoration-none
+          "
+    >
+      CRM Realty BBS
+    </Link>
+
+    <nav className="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
+      <li>
+        <Link to="/form" className="nav-link px-2 link-dark">Форма</Link>
+      </li>
+      <li>
+        <Link to="/table" className="nav-link px-2 link-dark">Таблица</Link>
+      </li>
+    </nav>
+
+    <div className="col-md-3 text-end">
+      <Button type="button" variant="outline-primary" className="me-2">Сотрудники</Button>
+      <Button type="button" variant="outline-danger">Выход</Button>
+    </div>
+  </header>
+);
+
+export default Header;
