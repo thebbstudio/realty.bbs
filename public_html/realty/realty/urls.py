6a59d43a3f423ff65bbe3f1b69b3ed6c6535e23b
Sergey <45151835+thebbstudio@users.noreply.github.com>
2022-06-05 16:37:11 +0500

    Обновление процедуры PutRealty

--- public_html/realty/realty/urls.py
+++ public_html/realty/realty/urls.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
     path('', include('web.urls'))
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
