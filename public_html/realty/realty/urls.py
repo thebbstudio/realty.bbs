05d6fa7ecef6248c79e15f5c1d6662206cdee968
Sergey <45151835+thebbstudio@users.noreply.github.com>
2022-06-05 16:45:00 +0500

    Update urls.py

--- public_html/realty/realty/urls.py
+++ public_html/realty/realty/urls.py
@@ -2,12 +2,13 @@ from django.contrib import admin
 from django.urls import path, include
 
 from django.conf.urls.static import static
+from django.conf import settings
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('web.urls')),
     path('api/', include('api.urls')),
-    path('', include('web.urls'))
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
